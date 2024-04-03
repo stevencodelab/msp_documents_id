dc596430a9cf99be7a9d9f4995dfd43a4484d531
stevenmorison <stevenmorizon123@gmail.com>
2024-04-03 09:55:00 +0700

    [UPD] add state and function
    [UPD 03-04-2024]
    1. add field state.
    2. add function for each state.
    3. add function for get data id.

--- models/msp_document_id.py
+++ models/msp_document_id.py
@@ -11,3 +11,26 @@ class MspDocumentsId(models.Model):
     revision_date = fields.Datetime(string="Revision Date", readonly=False)
     effective_date = fields.Datetime(string="Effective Date", readonly=False, store=True)
 
+    state = fields.Selection([
+            ('draft','Draft'),
+            ('confirmed','Confirmed'),
+            ('cancel', 'Cancelled')], 
+            string="Status", readonly=True,copy=False, default="draft",track_visibility='onchange', widget='statusbar')  
+
+    # Tombol Draft
+    def action_draft(self):
+        self.write({'state': 'draft'}) 
+    # Tombol start
+    def action_confirmed(self):
+        self.write({'state': 'confirmed'})
+    # Tombol Cancel
+    def action_cancel(self):
+        self.write({'state': 'cancel'})       
+
+    def name_get(self):
+        result = [] 
+        for record in self:
+            id = record.documents_id 
+            result.append((record.id, id))
+        return result                     
+    
